87dde98cfe3487120e43d411b1dce4b6ed7dbd7d
MohamedMassoud10 <massoudfcai@gmail.com>
2023-09-07 10:36:11 +0300

    added header styles and customization

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 import "./header.css";
 const Navbar = () => {
-  return <div className="container">Hello from Header</div>;
+  return (
+    <div className="container">
+      <header>
+        <div className="logo">
+          <h1>Admin dashboard</h1>
+        </div>
+        <div className="avatar">
+          <img
+            src="./../../images/icon.png"
+            alt="Icon"
+            className="avatar-img"
+          />
+        </div>
+      </header>
+      <hr />
+    </div>
+  );
 };
 
 export default Navbar;
