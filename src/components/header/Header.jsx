ff72e5c312d607966d83d4c2acb6b9863acc7b94
MohamedMassoud10 <massoudfcai@gmail.com>
2023-09-07 09:55:46 +0300

    added components ,  pages folders and installed react-router and redudux lib as a project setup

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./header.css";
+const Navbar = () => {
+  return <>Hello from Header</>;
+};
+
+export default Navbar;
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,11 @@
+import Header from "./components/header/Header";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <>
-    <div>Hello</div>
+    <Header />
   </>
 );
