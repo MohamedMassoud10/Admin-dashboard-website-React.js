df69d54c87301182084c6d1fd558661771dffb3c
MohamedMassoud10 <massoudfcai@gmail.com>
2023-09-09 03:35:47 +0300

    added Login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,35 @@
-import React from "react";
-import AdsCollection from "./components/AdsCollection";
-import { Route, Routes } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Header from "./components/Header";
-import "./globalstyles.css";
+import AdsCollection from "./components/AdsCollection";
 import NewAds from "./features/newAds";
 import UpdateAds from "./features/updateAds";
+import Login from "./pages/login/Login";
+import "./globalstyles.css";
 const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    //navigate("/login");
+  }, []);
+
   return (
-    <>
-      <Header />
-      <Routes>
-        <Route path="/" element={<AdsCollection />}></Route>
-        <Route path="/newAds" element={<NewAds />}></Route>
-        <Route path="/updateAds/:id" element={<UpdateAds />}></Route>
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route
+        path="/"
+        element={
+          <>
+            <Header />
+            <Routes>
+              <Route path="/" element={<AdsCollection />} />
+              <Route path="/newAds" element={<NewAds />} />
+              <Route path="/updateAds/:id" element={<UpdateAds />} />
+            </Routes>
+          </>
+        }
+      />
+    </Routes>
   );
 };
 
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import "./login.css";
+import { useNavigate } from "react-router-dom";
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const handleLogin = () => {
+    navigate("/");
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form>
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="button" onClick={handleLogin}>
+          Login
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
